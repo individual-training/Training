17249e92d8afa4a08ea87685b639eb59a596124a
pangchao <pangchao@zetyun.com>
2020-04-28 10:58:36 +0800

    添加首屏图片

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter, Link , connect } from 'umi';
+import { withRouter, Link, connect } from 'umi';
 import { Breadcrumb, Layout, Row, Col, Modal } from 'antd';
 import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import breadConfig from '@/config/breadCrumb.js';
@@ -8,95 +8,120 @@ import styles from './index.less';
 const { Header, Content } = Layout;
 const { confirm } = Modal;
 
-
-@connect(({global})=>({global}))
+@connect(({ global }) => ({ global }))
 @withRouter
-export default class appLayout extends React.Component{
-  constructor(props){
+export default class appLayout extends React.Component {
+  constructor(props) {
     super(props);
+    this.state = {
+      showImg: true,
+    };
   }
 
   componentDidMount() {
     this.props.dispatch({
-      type:'global/Init'
-    })
+      type: 'global/Init',
+    });
+    setTimeout(() => {
+      this.setState({
+        showImg: false,
+      });
+    }, 1.5 * 1000);
   }
 
   logout = () => {
     confirm({
       content: '是否要退出登录？',
       onOk() {
-        window.location.href = '/login'
+        window.location.href = '/login';
       },
-      okText:"确认",
-      cancelText:'取消'
+      okText: '确认',
+      cancelText: '取消',
     });
-  }
+  };
 
   getBreadcrumb = () => {
-    const { location: { pathname } } = this.props;
+    const {
+      location: { pathname },
+    } = this.props;
     const locArr = pathname.split('/');
     locArr.shift();
-    if(locArr.length>0){
+    if (locArr.length > 0) {
       return (
         <Breadcrumb separator=">">
           <Breadcrumb.Item>
             <Link to={'/'}>首页</Link>
           </Breadcrumb.Item>
-          {
-            locArr.map(item =>{
-              return breadConfig[item] ? (
-                <Breadcrumb.Item>{breadConfig[item].title}</Breadcrumb.Item>
-              ) : '';
-            })
-          }
-
+          {locArr.map(item => {
+            return breadConfig[item] ? (
+              <Breadcrumb.Item>{breadConfig[item].title}</Breadcrumb.Item>
+            ) : (
+              ''
+            );
+          })}
         </Breadcrumb>
-      )
+      );
     }
-  }
+  };
 
-  render(){
-    const { children, location: { pathname }, global } = this.props;
+  render() {
+    const {
+      children,
+      location: { pathname },
+      global,
+    } = this.props;
     const { userInfo } = global;
-    if(pathname === '/' || pathname === '/login'){
-      return children
+    if (this.state.showImg && pathname == '/') {
+      return (
+        <div style={{ width: '100%', height: '100%', backgroundColor: 'grey' }}>
+          <img
+            style={{ width: '100%', height: '100%' }}
+            src={require('../static/images/indexImg.jpg')}
+            alt=""
+          />
+        </div>
+      );
+    }
+    if (pathname === '/' || pathname === '/login') {
+      return children;
     }
     return (
       <div className={styles.index}>
         <Layout className={styles.layout}>
           <Header className={styles.header}>
             <Row>
-              <Col span={6} offset={2} style={{textAlign:'left'}}>
-                <img className={styles.headerImg} src={require('../static/images/junHui.png')} alt=""/>
-                <span className={styles.headerTitle} >单兵综合体能训练系统</span>
+              <Col span={6} offset={2} style={{ textAlign: 'left' }}>
+                <img
+                  className={styles.headerImg}
+                  src={require('../static/images/junHui.png')}
+                  alt=""
+                />
+                <span className={styles.headerTitle}>单兵综合体能训练系统</span>
               </Col>
               <Col span={4} offset={6}>
-                <UserOutlined/>
-                <span style={{marginLeft:20}}>{userInfo.username}</span>
+                <UserOutlined />
+                <span style={{ marginLeft: 20 }}>{userInfo.username}</span>
               </Col>
               <Col span={3}>
-                <span style={{fontSize:14,cursor:'pointer'}}>
-                  <LogoutOutlined style={{color:'red'}}/>
-                  <span style={{marginLeft:6}} onClick={this.logout}>退出</span>
+                <span style={{ fontSize: 14, cursor: 'pointer' }}>
+                  <LogoutOutlined style={{ color: 'red' }} />
+                  <span style={{ marginLeft: 6 }} onClick={this.logout}>
+                    退出
+                  </span>
                 </span>
               </Col>
             </Row>
           </Header>
           <Content>
-            <Row style={{height:'100%',marginTop:0}}>
+            <Row style={{ height: '100%', marginTop: 0 }}>
               <Col offset={2} span={20} className={styles.content}>
-                <div className={styles.bread}>
-                  {this.getBreadcrumb()}
-                </div>
-                <div className={styles.children}>
-                  { children}
-                </div>
+                <div className={styles.bread}>{this.getBreadcrumb()}</div>
+                <div className={styles.children}>{children}</div>
               </Col>
             </Row>
           </Content>
         </Layout>
       </div>
-    )
+    );
   }
 }
